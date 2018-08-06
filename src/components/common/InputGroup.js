42a202334854ec58057a78b991ea3133f72c69e9
Fadi Saadeldin <fadi@Fadis-MacBook.local>
2018-08-06 20:44:30 +0300

    prepare to deploy

--- src/components/common/InputGroup.js
+++ src/components/common/InputGroup.js
@@ -3,21 +3,40 @@ import classnames from 'classnames';
 import PropTypes from 'prop-types';
 
 const InputGroup = ({
+  name,
+  placeholder,
   value,
-    onClick,
-    iconClass
+  type,
+  onChange,
+  defaultValue
 }) => {
-    return (
-        <div className="input-group ml-2 ">
-            <button
-                type="button"
-                onClick={onClick}
-                className={iconClass}
-            >{value}
-            </button>
-
-        </div>
-    );
+  return (
+    <div className="input-group ml-5 ">
+      <input
+        className="form-control form-control-lg"
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onChange={onChange}
+        defaultValue={defaultValue}
+      />
+    
+    </div>
+  );
 };
 
-export { InputGroup };
+InputGroup.propTypes = {
+  name: PropTypes.string.isRequired,
+  placeholder: PropTypes.string,
+  value: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+  error: PropTypes.string,
+  type: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired
+};
+
+InputGroup.defaultProps = {
+  type: 'text'
+};
+
+export {InputGroup};
